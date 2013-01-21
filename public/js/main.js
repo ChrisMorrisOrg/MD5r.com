80155d974854484316e0add006b352682c31a641
Chris <chris@chrismorris.org>
2013-01-21 21:16:28 +1100

    v2.0.011BG

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
 alert("main loaded");
-if(jQuery){
-    alert("jQuery loaded.");
-}
-
-$(document).ready(function(){
-    # If there's a text input available, focus on it.
-    $("input:text").focus();
-});
