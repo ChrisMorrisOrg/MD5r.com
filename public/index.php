a4863493797f890854855d2e338ae066d3b3b67f
Chris <chris@chrismorris.org>
2013-01-21 21:13:51 +1100

    v2.0.011BD

--- public/index.php
+++ public/index.php
@@ -78,14 +78,8 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
 
-        <script src="/js/main.js"></script>
+        <script src="/js/main.js" type="text/javascript">alert("embed loaded.");</script>
 
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
     </body>
 </html>
 <?php
